0c3fc8de7986e4f88eb115732d9d773b9155dfd2
Tarunthakurexe <106328265+Tarunthakurexe@users.noreply.github.com>
2025-09-18 04:16:03 +0530

    updated project

--- src/Pokemon.jsx
+++ src/Pokemon.jsx
@@ -1,8 +1,14 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./index.css";
+import { PokemonCards } from "./PokemonCards";
+
 export const Pokemon = () => {
-    
-    const API = "https://pokeapi.co/api/v2/pokemon/?offset=24&limit=24";
+    const [pokemon, setpokemon] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(false);
+    const [search, setSearch] = useState("");
+
+    const API = "https://pokeapi.co/api/v2/pokemon/?offset=0&limit=124";
 
     const fetchPokemon = async() => {
         try{
@@ -15,9 +21,14 @@ export const Pokemon = () => {
                 const data = res.json();
                 return data;
             });
+        const detaildResponses = await Promise.all(detailedPokemonData);
+        setpokemon(detaildResponses);
+        setLoading(false);
 
         }catch(error){
             console.log(error);
+            setLoading(false);
+            setError(error);
         }
     }
 
@@ -25,6 +36,42 @@ export const Pokemon = () => {
         fetchPokemon();
     }, []);
 
-    return <h1>catch them all</h1>
+    const searchData = pokemon.filter((curPokemon) => 
+        curPokemon.name.toLowerCase().includes(search.toLowerCase())
+    );
+
+    if(loading){
+        return (<div>
+            <h1>Loading...</h1>
+        </div>);
+    }
+    if(error){
+        return (
+            <div>
+                <h1>{error.message}</h1>
+            </div>
+        );
+    }
+
+    return (
+        <section className="container">
+            <header>
+                <h1>Catch Them All!</h1>
+            </header>
+            <div className="pokemon-search">
+                <input type="text" placeholder="Search" value = {search} onChange = {(e)=> setSearch(e.target.value)}/>
+            </div>
+            <div>
+                <ul className="cards">
+                    {
+                        searchData.map((curPokemon) => {
+                            return <PokemonCards key = {curPokemon.id} pokemonData = {curPokemon}/>
+                        })
+                    }
+                </ul>
+            </div>
+        </section>
+
+    );
 
 }
